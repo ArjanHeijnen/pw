3260cbeedc16c912e397ab0aa5d9786e6e529835
unknown <arjanheijnen@outlook.com>
2021-11-05 11:21:35 +0100

    fix

--- sw.js
+++ sw.js
@@ -23,3 +23,44 @@ self.addEventListener('push', function (e) {
         self.registration.showNotification('Hello world!', options)
     );
 });
+
+self.addEventListener('install', e => {
+    console.log('install');
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            console.log('cache');
+            return cache.addAll([
+                './',
+                '/images/ArrowLeft.png']);
+        })
+    )
+});
+
+self.addEventListener('fetch', e => {
+    console.log(`intercepting ${e.request.url}`);
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    )
+})
+
+self.addEventListener('notificationclick', event => {
+    const notification = event.notification;
+    const action = event.action;
+    const primaryKey = notification.data.primaryKey;
+    console.log('Notification: ', primaryKey);
+    if (action === 'close') {
+        console.log('Close notification');
+        notification.close();
+    } else if (action === 'Somthingelse') {
+        console.log('Go notification');
+        clients.openWindow('https://www.sdinternational.nl/');
+    } else if (action === 'go') {
+        console.log('Go notification');
+        clients.openWindow('http://imbuildings.com');
+    } else {
+        console.log('Go notification');
+        clients.openWindow('http://google.com');
+    }
+})
