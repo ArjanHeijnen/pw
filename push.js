b6ad0f5945f421274697a729835c7c9153c2c83e
unknown <arjanheijnen@outlook.com>
2021-11-05 11:14:57 +0100

    fix

--- push.js
+++ push.js
@@ -3,18 +3,12 @@ var push = require('web-push');
 let vapidkeyes = //push.generateVAPIDKeys();
 //console.log(vapidkeyes);
 {
-    publicKey: 'BNRro0yvR4UoTCvjZr9njT28AMyfUvSuPtCF_cLlN-wm5KT7GfCEPjdOdcKc9-wYUhJkeSIVFryPb34Ts8D2R-0',
-    privateKey: 'T7kzJ_C7xCnpy63_N8KsxhDBWuSU1xVkDIL9JCiZWOU'
+    publicKey: 'BNRro0yvR4UoTCvjZr9njT28AMyfUvSuPtCF_cLlN-wm5KT7GfCEPjdOdcKc9-wYUhJkeSIVFryPb34Ts8D2R-0',// used on index.html
+    privateKey: 'T7kzJ_C7xCnpy63_N8KsxhDBWuSU1xVkDIL9JCiZWOU'// should be hidden
 };
 
-push.setVapidDetails('mailto:arjan.heijnen@student.nhlstenden.com', vapidkeyes.publicKey, vapidkeyes.privateKey);
+push.setVapidDetails('mailto:arjan.heijnen@student.nhlstenden.com', vapidkeyes.publicKey, vapidkeyes.privateKey);//idk why using my own email, in example a fake email is used
 
 //get sub data from database or something
-let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/esFxAY7optk:APA91bFH7qgtygJUrFvFeh9ZV_d1R4HOELJ9vbsWiDD_eXZgYotiKF_bb6BrE_t7mbJ0avUoULNt5QVjmb85fEB4PaKZPuFWSPHVOZx7zsSgvqSoCmF5y3i2_UkET1u7Y1yS7ELdVOk8", "expirationTime": null,
-    "keys": {
-        "p256dh": "BKdoo7ewmQksrle6lLZGtw23_c8UZOHcrie7EUVxaQNRDn4fyp0Bi7gROxidvFlPOHuHfzXZZ3dRfXo6iClZ5yg",
-        "auth": "ChfUDUjoLAK0f_-UNgYNHA"
-    }
-}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cR29n8_e_ds:APA91bHo2bisoJkdkmbrEIRrS-nC7F2K8AsBPBGSxfZXtQtU9M9TZ3vQ8itjbY4eAMisF--_dDrtM5E5BaiqHRVvATBWXfHYae1B1oyXqvT_RUxiX-a0M5E-f_3NJOFswmTrrkJge3gP","expirationTime":null,"keys":{"p256dh":"BMATtNS8kS6UxJ7IC2lWoOAGlArj81B3lEv-163hPgeLRlvirwDB0IXGlPXuqApqEivjLW2itOF6wBpZoxwsV2w","auth":"_6qjEr2buFnw3zLzfEl9Qg"}}
 push.sendNotification(sub, 'test message');
--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('push', function (e) {
     var options = {
         body: 'This notification was generated from a push!',
-        icon: 'images/example.png',
-        vibrate: [100, 50, 100],
+        icon: 'images/ArrowLeft.png',
+        vibrate: [1000, 500, 1000],
         data: {
             dateOfArrival: Date.now(),
             primaryKey: '2'
@@ -18,7 +18,7 @@ self.addEventListener('push', function (e) {
             },
         ]
     };
-    
+
     e.waitUntil(
         self.registration.showNotification('Hello world!', options)
     );
