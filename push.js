babe4e33b4922b90c9cacb663669e7afd8fa21d6
unknown <arjanheijnen@outlook.com>
2021-11-05 10:59:02 +0100

    fix2

--- push.js
+++ push.js
@@ -9,9 +9,9 @@ let vapidkeyes = //push.generateVAPIDKeys();
 
 push.setVapidDetails('mailto:arjan.heijnen@student.nhlstenden.com', vapidkeyes.publicKey, vapidkeyes.privateKey);
 
+//get sub data from database or something
 let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/esFxAY7optk:APA91bFH7qgtygJUrFvFeh9ZV_d1R4HOELJ9vbsWiDD_eXZgYotiKF_bb6BrE_t7mbJ0avUoULNt5QVjmb85fEB4PaKZPuFWSPHVOZx7zsSgvqSoCmF5y3i2_UkET1u7Y1yS7ELdVOk8",
-    "expirationTime": null,
+    "endpoint": "https://fcm.googleapis.com/fcm/send/esFxAY7optk:APA91bFH7qgtygJUrFvFeh9ZV_d1R4HOELJ9vbsWiDD_eXZgYotiKF_bb6BrE_t7mbJ0avUoULNt5QVjmb85fEB4PaKZPuFWSPHVOZx7zsSgvqSoCmF5y3i2_UkET1u7Y1yS7ELdVOk8", "expirationTime": null,
     "keys": {
         "p256dh": "BKdoo7ewmQksrle6lLZGtw23_c8UZOHcrie7EUVxaQNRDn4fyp0Bi7gROxidvFlPOHuHfzXZZ3dRfXo6iClZ5yg",
         "auth": "ChfUDUjoLAK0f_-UNgYNHA"
